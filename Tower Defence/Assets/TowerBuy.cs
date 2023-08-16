e275f716c47a639009419e608e7ce9f92e877d30
CooperHampson <132439269+CooperHampson@users.noreply.github.com>
2023-08-16 16:10:25 +0800

    Start Tower Placement

---
+++ Tower Defence/Assets/TowerBuy.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TowerBuy : MonoBehaviour
+{
+
+    public Button Tower1Button;
+
+    void SwordTowerPurchase()
+    {
+        if (Tower1Button)
+        {
+            gameObject.
+        }
+    }
+}
